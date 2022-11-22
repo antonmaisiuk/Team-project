f1172b4c52fea00c73c98ccc3f7f299ac650d33e
Adi2000git <104164478+Adi2000git@users.noreply.github.com>
2022-11-22 22:11:46 +0100

    Dodaj transakcje

---
+++ Transaction.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Elaborate
+{
+    public class Transaction
+    {
+            public int Id { get; set; }
+            public DateAndTime date { get; set; }
+            public float value { get; set; }
+            public string title { get; set; }
+            public string comment { get; set; }
+            public TransCategoryEnum category { get; set; }
+
+            public Transaction() { }      
+    }
+}
