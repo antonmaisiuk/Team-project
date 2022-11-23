90acb6cb0db40fd687872c15698c4c64a447546e
Anton Maisiuk <63475333+antonmaisiuk@users.noreply.github.com>
2022-11-23 18:35:08 +0100

    Dodawanie klasy transakcje (#4)
    Co-authored-by: Adi2000git <104164478+Adi2000git@users.noreply.github.com>

---
+++ TransCategoryEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace Elaborate
+{
+    public class TransCategoryEnum
+    {
+        enum id
+        {
+            IdAkcji,
+            IdKryptowaluty,
+            IdSurowca
+        }
+        enum CategoryName
+        {
+            Akcje,
+            Kryptowaluty,
+            Surowce
+        }
+    }
+}
