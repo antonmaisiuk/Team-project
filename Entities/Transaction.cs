e172d84cbd5bf2bcea0114271a0a14335c510f37
KubaW <jackobemc2@gmail.com>
2022-12-02 15:36:17 +0100

    dodanie konstruktora do klasy Account

--- Entities/Transaction.cs
+++ Entities/Transaction.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -9,6 +10,32 @@ namespace Elaborate.Elaborate.Entities
 {
     public class Transaction
     {
+            public Transaction(DateTime date, decimal value, string title, string comment,  int accountId)
+            {
+            this.Date = date;
+            this.Value = value;
+            this.Title = title;
+            this.Comment = comment;
+            this.AccountId = accountId;
+            //this.TransCategoryId = transactionCatId;
+                try
+                {
+                    string mySqlConnectionString = "server=146.59.126.32;port=3306;uid=user;pwd=Yg5udzLxxw9ADsT;database=elaborate-db";
+
+                    using var con = new MySqlConnection(mySqlConnectionString);
+                    con.Open();
+                    string query = "SELECT MAX(Id) FROM Transactions ";
+                    var cmd = new MySqlCommand(query, con);
+                    var maxId = Int32.Parse(cmd.ExecuteScalar().ToString());
+                    this.Id = maxId + 1;
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
             [Key]
             public int Id { get; set; }
             [Display(Name = "Data stworzenia Transakcji")]
