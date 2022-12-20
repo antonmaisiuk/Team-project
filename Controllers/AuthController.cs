be2795b7f7203b88cc41ba911f4a89b4d9f027ed
Marcin Konopka <m-konopka815@wp.pl>
2022-12-20 15:37:23 +0100

    Pierwsze 15 minut tutoriala Antona

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elaborate.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Hello()
+        {
+            return Ok("success");
+        }
+    }
+}
