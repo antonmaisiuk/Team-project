194d21b018f0741b727e33e030f64a89f7788c6a
Marcin Konopka <m-konopka815@wp.pl>
2023-01-10 00:37:43 +0100

    Update InvestmentPreciousMetals.cs

--- Entities/InvestmentPreciousMetals.cs
+++ Entities/InvestmentPreciousMetals.cs
@@ -1,11 +1,49 @@
-﻿using System;
+﻿using Elaborate.Elaborate.Entities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Elaborate.Entities
 {
-    public class Investment
+    public class InvestmentPreciousMetals
     {
+        private decimal _metalRate = 299.99m; 
+
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Ilość metalu w gramach")]
+        [Required]
+        public double Amount { get; set; }
+        /// <summary>
+        /// Id konta do którego należy inwestycja
+        /// </summary>
+        public int AccountId { get; set; }
+        public virtual Account Account { get; set; }
+        [Display(Name = "Typ metalu")]
+        [Required]
+        public int TypePreciousMetalId { get; set; }
+        public virtual TypePreciousMetal TypePreciousMetal { get; set; }
+        public decimal ValueOfInvestment 
+        {
+            get 
+            {
+                decimal decimalAmount = Convert.ToDecimal(Amount);
+                return _metalRate * decimalAmount; 
+            } 
+        }
+
+
+
+
+        public InvestmentPreciousMetals(double amount, int accountId, int typePreciousMetalId)
+        {
+            this.Amount = amount;
+            this.AccountId = accountId;
+            this.TypePreciousMetalId = typePreciousMetalId;
+        }
     }
 }
