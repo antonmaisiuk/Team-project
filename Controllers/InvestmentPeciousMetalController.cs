eae2ac3d6f8cc35b6e97ccace30f0236e29ee651
PatrykWitk <106755927+PatrykWitk@users.noreply.github.com>
2023-01-10 14:22:44 +0100

    Create InvestmentPeciousMetalController.cs
    Utworzenie kontrolera inwestycji + pobranie listy inwestycji zalogowanego uzytkownika

---
+++ Controllers/InvestmentPeciousMetalController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Elaborate.Elaborate.Entities;
+using Elaborate.Entities;
+using Elaborate.Helpers;
+using Elaborate.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elaborate.Controllers
+{
+    [Route("api/[controller]")]
+    public class InvestmentPeciousMetalController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly JwtService _jwtService;
+
+        public InvestmentPeciousMetalController(ApplicationDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+  
+        [Route("investments")]
+        public ActionResult<IEnumerable<InvestmentPreciousMetals>> GetAll()
+        {
+            var jwt = Request.Cookies["jwt"];
+
+            var token = _jwtService.Verify(jwt);
+
+            int userId = int.Parse(token.Issuer);
+
+            var investmentsPreciousMetal = _dbContext
+            .InvestmentsPreciousMetals.Where(r => r.Account.Id == userId)
+            .ToList();
+
+            return Ok(investmentsPreciousMetal);
+        }
+    }
+}
