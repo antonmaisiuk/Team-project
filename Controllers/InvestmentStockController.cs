39e801a981f99d2c270dee422cb37e0d1cc812ad
Marcin Konopka <m-konopka815@wp.pl>
2023-03-09 11:26:07 +0100

    Poprawa StockController i dodanie modelu

--- Controllers/InvestmentStockController.cs
+++ Controllers/InvestmentStockController.cs
@@ -36,7 +36,7 @@ namespace Elaborate.Controllers
             int userId = int.Parse(token.Issuer);
 
             var investmentStocks = _dbContext
-            .InvestmentStock.Where(r => r.Account.Id == userId)
+            .InvestmentStocks.Where(r => r.Account.Id == userId)
             .ToList();
 
             return Ok(investmentStocks);
@@ -45,11 +45,11 @@ namespace Elaborate.Controllers
         [HttpPost]
         public ActionResult<InvestmentStock> DeleteStock(int stockId)
         {
-            var stockToDelete = _dbContext.InvestmentStock.SingleOrDefault(t => t.Id == stockId);
+            var stockToDelete = _dbContext.InvestmentStocks.SingleOrDefault(t => t.Id == stockId);
 
             if (stockToDelete != null)
             {
-                _dbContext.InvestmentStock.Remove(stockToDelete);
+                _dbContext.InvestmentStocks.Remove(stockToDelete);
                 _dbContext.SaveChanges();
                 return Ok(stockToDelete);
             }
@@ -66,22 +66,22 @@ namespace Elaborate.Controllers
             var userId = int.Parse(token.Issuer);
             stock.AccountId = userId;
 
-            stock.StockCategoryId = 1; 
-            _dbContext.InvestmentStock.Add(stock);
+            stock.TypeStockId = 1; 
+            _dbContext.InvestmentStocks.Add(stock);
             _dbContext.SaveChanges();
 
             var stockList = _dbContext
-                .InvestmentStock.Where(r => r.Account.Id == userId)
+                .InvestmentStocks.Where(r => r.Account.Id == userId)
                 .ToList();
 
-            decimal stockSum = stockList.Sum(c => c.ValueOfInvestment);
+           // decimal stockSum = stockList.Sum(c => c.ValueOfInvestment);    Chwilowo zakomentowany M.K
 
-            Object[] resultArr = new Object[] { stock, stockSum };
+            Object[] resultArr = new Object[] { stock, /*stockSum*/ };
 
             return Ok(resultArr);
         }
 
-        [HttpGet("StocksSum")]
+        /*[HttpGet("StocksSum")]   Chwilowo zakomentowany M.K
         public ActionResult<InvestmentStock> GetSumOfStocks()
         {
             var jwt = Request.Cookies["jwt"];
@@ -92,7 +92,7 @@ namespace Elaborate.Controllers
                 .InvestmentStock.Where(r => r.Account.Id == userId).Sum(c => c.ValueOfInvestment);
 
             return Ok(StocksSum);
-        }
+        }*/
     }
 }
 
