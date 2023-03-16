2bdc6f4bd129ec0974ff9b50e57593de31615f0f
Marcin Konopka <m-konopka815@wp.pl>
2023-03-16 13:12:48 +0100

    Poprawki

--- Models/Create/CreateInvestmentPreciousMetalsDto.cs
+++ Models/Create/CreateInvestmentPreciousMetalsDto.cs
@@ -18,7 +18,7 @@ namespace Elaborate.Models
         public virtual Account Account { get; set; }
         [Display(Name = "Typ metalu")]
         [Required]
-        public int TypePreciousMetalId { get; set; }
+        public int TypeId { get; set; }
         public virtual TypePreciousMetal TypePreciousMetal { get; set; }
     }
 }
